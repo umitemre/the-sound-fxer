3a3ba60d8d33bba1e25cf6ab80af5f5395c9f822
Emre Aydin <emreaydin149@gmail.com>
2018-01-22 23:23:05 +0300

    UI butonlarına işlev eklendi.

--- app/src/main/java/net/cizirti/thesoundfxer/adapter/SoundFXAdapter.java
+++ app/src/main/java/net/cizirti/thesoundfxer/adapter/SoundFXAdapter.java
@@ -74,25 +74,36 @@ public class SoundFXAdapter extends RecyclerView.Adapter<SoundFXAdapter.ViewHold
         final SoundFX fx = soundFXES.get(position);
         final Timer timer;
 
-        // holder.tv_remove.setVisibility(App.isEditMode() ? View.VISIBLE : View.INVISIBLE);
-
         // parse path
         String[] paths = fx.getPath().split("/");
 
         // set file name as object item name
         holder.tv_soundname.setText(paths[paths.length - 1]);
 
-        // add on click event
-        holder.tv_soundname.setOnClickListener(new OnSoundFXClickListener(playerMap.get(fx.getId()), holder) {
+        // add on click play event
+        View.OnClickListener sfx_play = new OnSoundFXClickListener(playerMap.get(fx.getId()), holder) {
             @Override
             public void onClick(View v) {
                 if (getMediaPlayer().isPlaying()) {
                     getMediaPlayer().pause();
+                    holder.ib_play.setImageDrawable(
+                            context.getResources().getDrawable(
+                                    R.drawable.ic_play_arrow
+                            )
+                    );
                 } else {
                     getMediaPlayer().start();
+                    holder.ib_play.setImageDrawable(
+                            context.getResources().getDrawable(
+                                    R.drawable.ic_pause
+                            )
+                    );
                 }
             }
-        });
+        };
+
+        holder.tv_soundname.setOnClickListener(sfx_play);
+        holder.ib_play.setOnClickListener(sfx_play);
 
         // set volume of the music
         playerMap.get(fx.getId()).setVolume(fx.getVolume(), fx.getVolume());
@@ -153,9 +164,31 @@ public class SoundFXAdapter extends RecyclerView.Adapter<SoundFXAdapter.ViewHold
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
+        holder.ib_replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSoundFXButtonClick(v, fx);
+            }
+        });
+
         holder.ib_delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                onSoundFXButtonClick(v, fx);
+            }
+        });
+
+        holder.tv_soundname.setTag(fx);
+
+        holder.tv_duration.setText(convertMsToReadableTime(
+                playerMap.get(fx.getId()).getDuration()
+        ));
+
+    }
+
+    private void onSoundFXButtonClick(final View v, final SoundFX fx) {
+        switch (v.getId()) {
+            case R.id.ib_delete:
                 AlertDialog.Builder alertDialog = new AlertDialog.Builder(context);
                 alertDialog.setTitle("Emin Misiniz?");
                 alertDialog.setMessage("Bu ses efektini silerseniz tekrar geri getiremeyeceksiniz. Bu ses efektini silmek gerçek dosyayı etkilemez.");
@@ -170,22 +203,28 @@ public class SoundFXAdapter extends RecyclerView.Adapter<SoundFXAdapter.ViewHold
                         Toasty.success(context, "Ses efekti başarıyla silindi.").show();
                     }
                 });
+
                 alertDialog.setNegativeButton("İPTAL", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
 
                     }
                 });
+
                 alertDialog.show();
-            }
-        });
+                break;
+            case R.id.ib_replay:
+                MediaPlayer mp = playerMap.get(fx.getId());
 
-        holder.tv_soundname.setTag(fx);
-
-        holder.tv_duration.setText(convertMsToReadableTime(
-                playerMap.get(fx.getId()).getDuration()
-        ));
-
+                // go back
+                mp.seekTo(0);
+                // play
+                mp.start();
+                break;
+            default:
+                // do nothing.
+                break;
+        }
     }
 
     private String convertMsToReadableTime(int duration) {
@@ -228,7 +267,7 @@ public class SoundFXAdapter extends RecyclerView.Adapter<SoundFXAdapter.ViewHold
         ProgressBar pb_played;
         SeekBar sb_volume;
 
-        ImageButton ib_delete;
+        ImageButton ib_delete, ib_replay, ib_play;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -238,6 +277,8 @@ public class SoundFXAdapter extends RecyclerView.Adapter<SoundFXAdapter.ViewHold
             sb_volume = itemView.findViewById(R.id.sb_volume);
             tv_duration = itemView.findViewById(R.id.tv_duration);
             tv_currentTime = itemView.findViewById(R.id.tv_currentTime);
+            ib_replay = itemView.findViewById(R.id.ib_replay);
+            ib_play = itemView.findViewById(R.id.ib_play);
             ib_delete = itemView.findViewById(R.id.ib_delete);
         }
     }
